fa4b1cb9813614f3c900127c138e2d0ae6db9988
Domenica <Domenica@Domenica>
2020-01-05 17:47:19 -0500

    Factura Cabecera y Detalle del proyecto

---
+++ src/ec/edu/ups/modelo/FCabecera.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Domenica
+ */
+public class FCabecera {
+    
+    private int ruc;
+    private Date fecha;
+    private double subtotal;
+    private double iva;
+    private double total;
+    
+  /*  //Clases
+    private Persona per;
+    private Producto pro;
+    private List<FDetalle> detalle;
+
+    public FCabecera() {
+        
+        detalle = new ArrayList<>();
+        fecha = new Date();
+        iva = 0.12;
+        
+    }
+
+    public FCabecera(int ruc, Date fecha, double subtotal, double iva, double total, Persona per, Producto pro, List<FDetalle> detalle) {
+        this.ruc = ruc;
+        this.fecha = fecha;
+        this.subtotal = subtotal;
+        this.iva = iva;
+        this.total = total;
+        this.per = per;
+        this.pro = pro;
+        this.detalle = detalle;
+    }
+
+    public int getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(int ruc) {
+        this.ruc = ruc;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Persona getPer() {
+        return per;
+    }
+
+    public void setPer(Persona per) {
+        this.per = per;
+    }
+
+    public Producto getPro() {
+        return pro;
+    }
+
+    public void setPro(Producto pro) {
+        this.pro = pro;
+    }
+
+    public List<FDetalle> getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(List<FDetalle> detalle) {
+        this.detalle = detalle;
+    }
+
+    @Override
+    public String toString() {
+        return "FCabecera{" + "ruc=" + ruc + ", fecha=" + fecha + ", subtotal=" + subtotal + ", iva=" + iva + ", total=" + total + ", per=" + per + ", pro=" + pro + ", detalle=" + detalle + '}';
+    }*/
+    
+}
---
+++ src/ec/edu/ups/modelo/FDetalle.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Domenica
+ */
+public class FDetalle {
+    
+    private int codigo;
+    private int cant;
+    private double ivaPro;
+    private double totalCP;
+    
+    //Clase
+    /*private Producto pro;
+
+    public FDetalle() {
+    }
+
+    public FDetalle(int codigo, int cant, double ivaPro, double totalCP, Producto pro) {
+        this.codigo = codigo;
+        this.cant = cant;
+        this.ivaPro = ivaPro;
+        this.totalCP = totalCP;
+        this.pro = pro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    public double getIvaPro() {
+        return ivaPro;
+    }
+
+    public void setIvaPro(double ivaPro) {
+        this.ivaPro = ivaPro;
+    }
+
+    public double getTotalCP() {
+        return totalCP;
+    }
+
+    public void setTotalCP(double totalCP) {
+        this.totalCP = totalCP;
+    }
+
+    public Producto getPro() {
+        return pro;
+    }
+
+    public void setPro(Producto pro) {
+        this.pro = pro;
+    }
+
+    @Override
+    public String toString() {
+        return "FDetalle{" + "codigo=" + codigo + ", cant=" + cant + ", ivaPro=" + ivaPro + ", totalCP=" + totalCP + ", pro=" + pro + '}';
+    }
+    */
+}
