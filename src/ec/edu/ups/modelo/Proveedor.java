60e286fda449f205c259a427b4f42cb0fa434888
Edison <Edison@MSI>
2020-01-07 21:55:43 -0500

    Producto, Proveedor

---
+++ src/ec/edu/ups/modelo/Proveedor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+/**
+ *
+ * @author Edison
+ */
+public class Proveedor {
+
+    private String id;
+    private String nombre;
+
+    public Proveedor() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "id=" + id + ", nombre=" + nombre + '}';
+    }
+
+}
