6e2b05c59401ef42cf0c838aa2bc3e46c9608812
Domenica <Domenica@Domenica>
2020-01-13 19:19:36 -0500

    Arreglo de clases

--- src/ec/edu/ups/modelo/Proveedor.java
+++ src/ec/edu/ups/modelo/Proveedor.java
@@ -13,15 +13,17 @@ public class Proveedor {
 
     private int id;
     private String nombre;
+    private String numFactura;
 
     public Proveedor() {
     }
 
-    public Proveedor(int id, String nombre) {
+    public Proveedor(int id, String nombre, String numFactura) {
         this.id = id;
         this.nombre = nombre;
+        this.numFactura = numFactura;
     }
-    
+
     public int getId() {
         return id;
     }
@@ -38,9 +40,17 @@ public class Proveedor {
         this.nombre = nombre;
     }
 
-    @Override
-    public String toString() {
-        return "Proveedor{" + "id=" + id + ", nombre=" + nombre + '}';
+    public String getNumFactura() {
+        return numFactura;
     }
 
+    public void setNumFactura(String numFactura) {
+        this.numFactura = numFactura;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "id=" + id + ", nombre=" + nombre + ", numFactura=" + numFactura + '}';
+    }
+    
 }
